833158f76a1b26e865e4ceb29fe558b23e2a770d
Benjamin Vitters <vittersdesign@hotmail.com>
2025-03-17 00:37:22 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <k-app theme="ios">
+    <k-page>
+      <k-navbar title="My Konsta App" />
+      <k-block strong>Hello world!</k-block>
+    </k-page>
+  </k-app>
+</template>
+
+<script>
+import { kApp, kPage, kNavbar, kBlock } from 'konsta/vue';
+
+export default {
+  components: {
+    kApp,
+    kPage,
+    kNavbar,
+    kBlock,
+  },
+};
+</script>
